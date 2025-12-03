d76a229bd4e2b6a22cb9c7ab748ef3184039fe46
sridurgas10 <76196643+sridurgas10@users.noreply.github.com>
2025-12-03 10:59:44 +0530

    Refactor dashboard route for clarity and consistency

--- dvdrental project/dashboard/route.py
+++ dvdrental project/dashboard/route.py
@@ -1,17 +1,13 @@
-
- 
 from fastapi import APIRouter
 from sqlalchemy.orm  import Session
-from dbconnection import get_db
+
 from fastapi import Depends
+from dbconnection import get_db
 from datetime import datetime
 from dashboard import crud,schema
 from typing import Optional
 
 dashboard_router=APIRouter(prefix="/dashboard",tags=["dashboard"])
-
-
-
 
 @dashboard_router.get("/v1/",response_model=schema.DashboardResponse) 
 async def dashboard(start_date: Optional[datetime]=None,
@@ -28,11 +24,11 @@ db:Session=Depends(get_db)):
     rental_by_category=crud.rentals_per_category(start_date,end_date,store_id,city_id,country_id,category_id,film_id,db)
     rental_per_day=crud.rentals_per_day(start_date,end_date,store_id,city_id,country_id,category_id,film_id,db)
     return {
-    "total_rentals": total_rentals,
-    "Total_Revenue": total_revenue,
-    "Top5_rented_movies": top5_rented_movie,
-    "Rentals_by_category": rental_by_category,
-    "Rentals_per_day": rental_per_day
+    "total_rentals": total_rentals ,
+    "total_revenue": total_revenue,
+    "top5_rented_movies": top5_rented_movie,
+    "rentals_by_category": rental_by_category,
+    "rentals_per_day": rental_per_day
 }
 
     
