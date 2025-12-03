06433445f981f18df38fb2d30c49ac46b766d8c4
sridurgas10 <76196643+sridurgas10@users.noreply.github.com>
2025-12-03 10:58:49 +0530

    Update schema.py

--- dvdrental project/dashboard/schema.py
+++ dvdrental project/dashboard/schema.py
@@ -2,52 +2,49 @@ from pydantic import BaseModel
 from datetime import datetime
 from typing import List
 
-
 class TotalRental(BaseModel):
-    store_id:int
-    city_id:int
-    country_id:int
-    film_id:int
-    total_rentals:int
+    store_id: int
+    city_id: int
+    country_id: int
+    film_id: int
+    total_rentals: int
 
 class TotalRevenue(BaseModel):
-    store_id:int
-    city_id:int
-    country_id:int
-    film_id:int
-    total_revenue:float
+    store_id: int
+    city_id: int
+    country_id: int
+    film_id: int
+    total_revenue: float
 
-class  Top5RentedMovie(BaseModel):
-    film_id:int
-    title:str
-    store_id:int
-    city_id:int
-    country_id:int
-    total_rentals:int
-
+class Top5RentedMovie(BaseModel):
+    film_id: int
+    title: str
+    store_id: int
+    city_id: int
+    country_id: int
+    total_rentals: int
 
 class RentalsCategory(BaseModel):
-    category_id:int
-    name:str
-    film_id:int
-    store_id:int
-    city_id:int
-    country_id:int
-    total_rentals:int
+    category_id: int
+    name: str
+    film_id: int
+    store_id: int
+    city_id: int
+    country_id: int
+    total_rentals: int
 
 class RentalsperDay(BaseModel):
-    category_id:int
-    film_id:int
-    store_id:int
-    city_id:int
-    country_id:int
+    category_id: int
+    film_id: int
+    store_id: int
+    city_id: int
+    country_id: int
     rental_date:datetime
-    total_rentals:int
-
+    total_rentals: int
 
 class DashboardResponse(BaseModel):
-    total_rentals:List[TotalRental]
-    Total_Revenue: List[TotalRevenue]
-    Top5_rented_movies:List[Top5RentedMovie]
-    Rentals_by_category:List[RentalsCategory]
-    Rentals_per_day:List[RentalsperDay]
+    total_rentals: List[TotalRental]
+    total_revenue: List[TotalRevenue]
+    top5_rented_movies: List[Top5RentedMovie]
+    rentals_by_category: List[RentalsCategory]
+    rentals_per_day:  List[RentalsperDay]
